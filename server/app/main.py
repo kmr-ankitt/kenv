f9fcb0a149a50ea8f9ccf3ac23c1700b2b500c97
ankit <sahuankit.murhu@gmail.com>
2025-04-09 10:23:13 +0530

    add: cors setup

--- server/app/main.py
+++ server/app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Secrets Vault API")
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the Secrets Vault API!"}
+
