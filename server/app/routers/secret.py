6f31a5046d6f7d8adf5682c43082eb24e2b4f62d
ankit <sahuankit.murhu@gmail.com>
2025-04-11 19:32:08 +0530

    add: Encrypting secrect

--- server/app/routers/secret.py
+++ server/app/routers/secret.py
@@ -3,16 +3,57 @@ from app.db.base import get_session
 from sqlmodel import Session, select
 from pydantic import BaseModel
 from datetime import datetime
+from app.utils.auth import get_current_user
+from app.models.user import User
+from app.models.secret import Secret as SecretModel
+from app.utils.security import encrypt_secret, decrypt_secret
 
 router = APIRouter(prefix="/secret", tags=["secret"])
+
 
 class Secret(BaseModel):
     name: str
     value: str
+    created_at: datetime
     expires_at: datetime
+
 
 @router.post("/")
 async def create_secret(
-        *, session: Session = Depends(get_session), request: Secret
+    *,
+    session: Session = Depends(get_session),
+    request: Secret,
+    current_user: User = Depends(get_current_user),
 ):
-    return ""
+    try:
+        # Find the current user by username
+        curr_user = session.exec(
+            select(User).where(User.username == current_user.username)
+        ).first()
+
+        if not curr_user:
+            raise HTTPException(status_code=404, detail="User not found")
+
+        # Encryption of secret value
+        encrypted_value = encrypt_secret(request.value)
+
+        # Create a new secret
+        new_secret = SecretModel(
+            owner_id=curr_user.id,
+            name=request.name,
+            value=encrypted_value,
+            created_at=request.created_at,
+            expires_at=request.expires_at,
+        )
+
+        session.add(new_secret)
+        session.commit()
+        session.refresh(new_secret)
+
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(status_code=500, detail=f"Secret creation failed: {str(e)}")
+
+    return {"message": "Secret created successfully", "secret": new_secret.name}
--- server/app/utils/security.py
+++ server/app/utils/security.py
@@ -1,6 +1,12 @@
+import os
 from passlib.context import CryptContext
+from cryptography import fernet
+from dotenv import load_dotenv
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+load_dotenv()
+SECRET_KEY = os.getenv("FERNET_KEY")
 
 
 def hash_password(password: str) -> str:
@@ -9,3 +15,15 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+def encrypt_secret(secret: str) -> str:
+    fernet_key = fernet.Fernet(SECRET_KEY)
+    encrypted_secret = fernet_key.encrypt(secret.encode())
+    return encrypted_secret.decode()
+
+
+def decrypt_secret(encrypted_secret: str) -> str:
+    fernet_key = fernet.Fernet(SECRET_KEY)
+    decrypted_secret = fernet_key.decrypt(encrypted_secret.encode())
+    return decrypted_secret.decode()
