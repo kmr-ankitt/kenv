0a138a61465cab172da815e83731e39a2500af9b
ankit <sahuankit.murhu@gmail.com>
2025-04-15 01:12:35 +0530

    add: Home page core

---
+++ client/src/app/home/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
+
+export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    if (!document.cookie.includes("access_token")) router.push("/login")
+  })
+  return (
+    <div className="h-screen w-full text-zinc-200">
+      Home
+    </div>
+  )
+}
