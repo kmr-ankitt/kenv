dc796e2bde4f289c39e084d587d90e9250b74853
ankit <sahuankit.murhu@gmail.com>
2025-04-14 11:08:34 +0530

    fix: cors error

--- server/app/main.py
+++ server/app/main.py
@@ -4,6 +4,7 @@ from app.db import base
 from app.routers.auth import routers as auth_routers
 from app.routers.secret import router as secret_router
 from app.routers.access_log import router as access_log_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -13,6 +14,23 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # Register all auth and secret routers
 for router in auth_routers:
