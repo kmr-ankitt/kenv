9b7285faae993a9a2bd4b49dccb4bd615f4efc51
ankit <sahuankit.murhu@gmail.com>
2025-04-18 09:54:41 +0530

    fix: Navigation fix

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,14 @@
 "use client"
 import { AnimatedButton } from "@/components/AnimatedButton";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(()=>{
+    if (document.cookie.includes("access_token")) router.push("/home")
+  })
   return (
     <div className="h-screen w-full flex gap-5 items-center justify-center">
       <Link href="/login">
